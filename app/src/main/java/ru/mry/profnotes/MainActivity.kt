fa9a1e9469c6608a6e4f963e84b63fa2c31d02ad
tchernets-maria <tchernatsmaria@yandex.ru>
2023-07-19 01:34:56 +0400

    Add Splash Screen Api

--- app/src/main/java/ru/mry/profnotes/MainActivity.kt
+++ app/src/main/java/ru/mry/profnotes/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import ru.mry.profnotes.databinding.ActivityMainBinding
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
